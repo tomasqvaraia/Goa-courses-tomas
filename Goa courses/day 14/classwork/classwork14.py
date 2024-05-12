c8afefd8e0a75135688a4f1be198655df1ce0c37
tomasqvaraia <158314848+tomasqvaraia@users.noreply.github.com>
2024-05-12 15:40:54 +0100

    Add files via upload

---
+++ Goa courses/day 14/classwork/classwork14.py
@@ -0,0 +1,18 @@
+#task1 
+name=input("enter your name")
+age=input("enter your age")
+print("hello "+name+" you are "+age)
+
+#task2
+name=input("enter your name")
+surname=input("enter your surname")
+age=input("enter your age")
+mom_name=input("enter your mom name")
+father_name=input("enter your father name")
+place=input("enter place where you live")
+print(name)
+print(surname)
+print(age)
+print(mom_name)
+print(father_name)
+print(place)
---
+++ Goa courses/day 14/homework/day 14.py
@@ -0,0 +1,19 @@
+#task1
+friends_name=input("enter your friends name")
+friends_name2=input("enter your friends name")
+friends_name3=input("enter your friends name")
+friends_name4=input("enter your friends name")
+print(friends_name)
+print(friends_name2)
+print(friends_name3)
+print(friends_name4)
+
+#task2
+mother_name=input("enter your mother name")
+father_name=input("enter your father name")
+print(mother_name)
+print(father_name)
+
+#task3
+favourite_food_name=input("enter your favourite food name")
+print(favourite_food_name)
